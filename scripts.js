856e115d4d9cde1f46dea2a3e024ae7d4ec448da
OtavoEXE <otaviodeoliveiraalbert@gmail.com>
2024-09-25 21:39:43 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// JavaScript para manipular comportamentos do site
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Mensagem enviada com sucesso! Vamos entrar em contato em breve.");
+});
